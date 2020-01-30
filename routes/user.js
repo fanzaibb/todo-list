57816c44966fcc877fb40d375107cf613a2c5ec3
Fanzai BB <your_email>
2020-01-30 14:04:23 +0800

    Add user registration feature

--- routes/user.js
+++ routes/user.js
@@ -19,7 +19,35 @@ router.get('/register', (req, res) => {
 
 //註冊檢查
 router.post('/register', (req, res) => {
-  res.send('register')
+  const { name, email, password, password2 } = req.body
+  // aka const name = req.body.name
+  User.findOne({ email: email }) //findOne為mongoose提供的方法
+      .then(user => { //若findOne執行無誤，繼續執行then裡的callback(?)
+        if (user) {
+          //若email已存在，不送出並回到註冊表單頁面
+          console.log('User already exists')
+          res.render('register', {
+            name,
+            email,
+            password,
+            password2
+          })
+        } else {
+          //若email不存在，新增使用者
+          const newUser = new User({
+            name,
+            email,
+            password
+          })
+          //新增完成後回首頁
+          newUser
+            .save()
+            .then(user => {
+              res.redirect('/')
+            })
+            .catch(err => console.log(err))
+        }
+  })
 })
 
 //登出
