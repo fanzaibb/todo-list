6d3f70311acd8018d544715d2cee1fabd57abc37
Fanzai BB <your_email>
2020-01-30 22:31:35 +0800

    Add user login feature using passport

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const passport = require('passport')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(passport.initialize())
+app.use(passport.session())
 
 
 //與資料庫連線的語法
